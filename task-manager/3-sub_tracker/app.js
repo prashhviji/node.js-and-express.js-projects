3e3a0bfafbd10ab7efcf66079761adb6ce514854
V.Prasanth <prasanth.v2023@vitstudent.ac.in>
2025-04-06 02:11:29 +0530

    init

--- task-manager/3-sub_tracker/app.js
+++ task-manager/3-sub_tracker/app.js
@@ -1,20 +1,13 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from 'express';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+app.get("/", (req, res) => {
+  res.send("Task Tracker");
+});
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
+export default app;
